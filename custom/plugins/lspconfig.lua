2dcb05ee08d964111f66fd9a976561812f7e5573
aiell0 <lawrence.aiello@laiello.com>
2023-05-17 22:06:26 -0400

    chore: add grammarly lsp

--- custom/plugins/lspconfig.lua
+++ custom/plugins/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = {"html", "tsserver", "dockerls", "jsonls", "terraformls", "tflint", "terraform_lsp", "eslint", "pylsp", "pyre", "pylyzer", "pyright", "golangci_lint_ls", "gopls"}
+local servers = {"html", "tsserver", "dockerls", "jsonls", "terraformls", "tflint", "terraform_lsp", "eslint", "pylsp", "pyre", "pylyzer", "pyright", "golangci_lint_ls", "gopls", "grammarly"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
