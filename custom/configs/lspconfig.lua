9e11bb5f8e5f5b56674d5f3c69724379d2c4ce8c
aiell0 <lawrence.aiello@laiello.com>
2023-06-29 21:40:34 -0400

    fix: terraform language server

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = {"html", "tsserver", "dockerls", "jsonls", "terraform_lsp", "tflint", "eslint", "pylsp", "pyre", "pylyzer", "pyright", "golangci_lint_ls", "gopls", "grammarly", "bashls", "angularls", "eslint", "gopls", "helm_ls", "java_language_server", "jsonls", "yamlls"}
+local servers = {"html", "tsserver", "dockerls", "jsonls", "terraformls", "tflint", "eslint", "pylsp", "pyre", "pylyzer", "pyright", "golangci_lint_ls", "gopls", "grammarly", "bashls", "angularls", "eslint", "gopls", "helm_ls", "java_language_server", "jsonls", "yamlls"}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
